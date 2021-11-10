83dabe86b42295403a6b09fff751b7a356224172
not1214 <91748221+not1214@users.noreply.github.com>
2021-11-10 15:33:16 +0900

    Update Book

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def edit
@@ -22,5 +25,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
